fb9defc0313806e4c0ac2a6e7c16ef81b5b0f49a
Jonathan Vareika <jp.vareika@gmail.com>
2020-11-16 15:18:09 +0000

    update files

---
+++ tests/tool_test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import mlproject
+import pandas as pd
+# Import from our lib
+from mlproject.tools import haversine
+import pytest
+def test_haversine():
+    lon1, lat1, lon2, lat2 = (48.865070, 2.380009, 48.235070, 2.393409)
+    out = haversine(lon1, lat1, lon2, lat2)
+    assert(out) == 70.00789153218594
